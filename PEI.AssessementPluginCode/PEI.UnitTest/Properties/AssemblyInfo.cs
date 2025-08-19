491ab33661de51aac4adaf7dc5d04396d1cd3e2b
Rishabh-Rishabh18 <187sonu@gmail.com>
2025-08-19 22:27:10 +0530

    Assembly Info

---
+++ PEI.AssessementPluginCode/PEI.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PEI.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Deloitte")]
+[assembly: AssemblyProduct("PEI.UnitTest")]
+[assembly: AssemblyCopyright("Copyright © Deloitte 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4e8532d7-0845-4163-a63e-872e3e89fb53")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
