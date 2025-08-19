ba5155043a932450fabffe6df5ef7fd3eca2af63
Rishabh-Rishabh18 <187sonu@gmail.com>
2025-08-19 23:48:28 +0530

    Unit test case Whitespaces

--- PEI.AssessementPluginCode/PEI.UnitTest/PreValidateContactTest.cs
+++ PEI.AssessementPluginCode/PEI.UnitTest/PreValidateContactTest.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using PEI.Assesment.Plugins;
 using Microsoft.Xrm.Sdk;
@@ -31,6 +31,37 @@ namespace PEI.UnitTest
                 Id = Guid.NewGuid()
             };
             target["emailaddress1"] = "test@test.com";
+
+            fakeXrm.PluginExecutionContext.InputParameters.Add("Target", target);
+
+
+            //Executing the plugin and asserting is there any invalid plugin execution
+            PreValidateCContact plugin = new PreValidateCContact();
+            InvalidPluginExecutionException ex = Assert.ThrowsException<InvalidPluginExecutionException>
+                (() =>
+                {
+                    fakeXrm.Context.ExecutePluginWith<PreValidateCContact>(fakeXrm.PluginExecutionContext);
+                });
+
+        }
+
+        [TestMethod]
+        public void expectPluginExceptionWhiteSpaceEmailProvided()
+        {
+            //initialize
+            fakeXrm.PluginExecutionContext.MessageName = "Create";// Create event
+            fakeXrm.PluginExecutionContext.Stage = 10;// Pre validate
+
+            // Initilazing the existing contact
+            Entity existingContact = new Entity { Id = Guid.NewGuid(), LogicalName = "contact", ["emailaddress1"] = "test@test.com" };
+            fakeXrm.Context.Initialize(new[] { existingContact });
+            //prepare the new contact data
+            Entity target = new Entity
+            {
+                LogicalName = "contact",
+                Id = Guid.NewGuid()
+            };
+            target["emailaddress1"] = " test@test.com ";
 
             fakeXrm.PluginExecutionContext.InputParameters.Add("Target", target);
 
